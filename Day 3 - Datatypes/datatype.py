3a017c2f8fe90ef60a387631987f16600eebdf37
tushitaparthasarathy <tushipy@gmail.com>
2026-05-02 21:05:14 +0400

    .

---
+++ Day 3 - Datatypes/datatype.py
@@ -0,0 +1,11 @@
+a=5
+print("Type of a: ", type(a))
+
+b=2.5
+print("Type of b: ", type(b))
+
+c= "coding"
+print("Type of c: ", type(c))
+
+d= True
+print("Type of d: ", type(d))
---
+++ Day 3 - Datatypes/stringoperation.py
@@ -0,0 +1,5 @@
+text=str(input("Enter a string: "))
+revText = text[::-1]
+text = revText
+print("Reverse of given string is : ")
+print(text)
