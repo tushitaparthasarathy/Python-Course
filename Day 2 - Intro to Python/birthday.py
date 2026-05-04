fc3e4ec44e46f59d006db2ead6570fac0b600331
tushitaparthasarathy <tushipy@gmail.com>
2026-05-04 19:53:29 +0400

    .

---
+++ Day 2 - Intro to Python/birthday.py
@@ -0,0 +1,11 @@
+friend_1="January 1"
+friend_2= "January 2"
+friend_3= "January 3"
+friend_4= "January 4"
+friend_5= "January 5"
+print(friend_1)
+print(friend_2)
+print(friend_3)
+print(friend_4)
+print(friend_5)
+
