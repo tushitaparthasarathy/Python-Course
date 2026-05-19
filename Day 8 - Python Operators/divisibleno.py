3024db55b6d693370bf4e46ef418006490083522
tushitaparthasarathy <tushipy@gmail.com>
2026-05-19 17:12:53 +0400

    .

---
+++ Day 8 - Python Operators/divisibleno.py
@@ -0,0 +1,9 @@
+print("Enter a number (numerator) :")
+numn=int(input())
+print("Enter a number (denominator) :")
+numd=int(input())
+if numn%numd==0:
+    print("\n" +str(numn)+ "is divisible by" +str(numd))
+else:
+    print("\n" +str(numn)+ "is not divisible by" +str(numd))
+    
---
+++ Day 8 - Python Operators/mean.py
@@ -0,0 +1,22 @@
+mean1 = 38
+wrong_number=36
+correct_number=56
+total_number=40
+
+
+sum = mean1*total_number
+print("the sum of 40 number: ", sum)
+8
+9
+10
+11
+12
+
+num2=sum-( (wrong_number)-(correct_number))
+print(" sum- ((wrong_number) - (correct_number)): ", num2)
+13
+
+14
+mean2=num2/total_number
+15
+print(mean2)
