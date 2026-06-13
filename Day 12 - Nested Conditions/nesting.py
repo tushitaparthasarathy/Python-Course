eae5d78c8e02e1793c3083e28ed9861f47e06215
tushitaparthasarathy <tushipy@gmail.com>
2026-06-13 21:16:35 +0400

    .

---
+++ Day 12 - Nested Conditions/nesting.py
@@ -0,0 +1,6 @@
+for i in range(1,21):
+    for j in range(i,i+1):
+        if j%2==0:
+            print(j,"Even")
+        else:
+            print(j,"Odd")
---
+++ Day 12 - Nested Conditions/prime.py
@@ -0,0 +1,12 @@
+num=int(input("Enter a number : "))
+if num>1:
+    for i in range(2,num):
+        if num%1==0:
+            print("Not a prime number")
+            break
+        else:
+            print("Prime number")
+else:
+    ("Not a prime number")
+        
+    
