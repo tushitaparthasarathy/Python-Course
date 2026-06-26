ffb9e85cd5fe3fd9c0fa9c02c545b2dbe1fe0e17
tushitaparthasarathy <tushipy@gmail.com>
2026-06-26 12:31:12 +0400

    .

--- Day 15 - Functions/calculator.py
+++ Day 15 - Functions/calculator.py
@@ -4,8 +4,8 @@ def subtract(P, Q):
    return P - Q   
 def multiply(P, Q):   
    return P * Q   
-def divide(P, Q):   
-   return P / Q 
+def divide(P, Q):
+   return P / Q  
 print ("Please select the operation.")    
 print ("a. Add")    
 print ("b. Subtract")    
