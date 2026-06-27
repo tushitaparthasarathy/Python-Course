03540b8f981b435b103ed7220f2d4aabd57a855e
tushitaparthasarathy <tushipy@gmail.com>
2026-06-27 22:21:27 +0400

    .

---
+++ Day 14 - Python test 1/homework.py
@@ -0,0 +1,14 @@
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor("lightblue")
+
+pen = turtle.Turtle()
+pen.color("red")
+pen.pensize(3)
+
+for i in range(4):
+    pen.forward(100)
+    pen.right(90)
+
+turtle.done()
