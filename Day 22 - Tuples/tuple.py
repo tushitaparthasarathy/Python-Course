121c181b3e88222db1e0451b3de207c918fc30a9
tushitaparthasarathy <tushipy@gmail.com>
2026-08-15 21:10:47 +0400

    .

---
+++ Day 22 - Tuples/tuple.py
@@ -0,0 +1,18 @@
+tuplex=("tuple", False, 3.8, 1)
+print(tuplex)
+
+tuplex=(1,2,3,4,5,6)
+print(tuplex)
+
+tuplex=tuplex+ (9,)
+print(tuplex)
+
+tuple1=(10,20,30,50,50)
+print(tuple1.count(50))
+
+tuplex=(1,2,3,4,5,6,7,8,9) 
+slice1=tuplex[3:6]
+print(slice1)
+
+slice1=tuplex[:6]
+print(slice1)
